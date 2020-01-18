3eac352935a8b258ac69ae90d673ff04c4702258
bokismoki <borisbosnjak084@gmail.com>
2020-01-18 14:50:23 +0100

    Init code cleaning

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,10 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - Social App',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -18,7 +21,7 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: false,
   /*
   ** Global CSS
   */
