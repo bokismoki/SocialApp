7d58981f21dc20e906f9f34b91eb98a9d3bc1633
bokismoki <borisbosnjak084@gmail.com>
2020-01-21 16:26:03 +0100

    Connected posts and comments to Profile/:id

--- components/CommentItem.vue
+++ components/CommentItem.vue
@@ -2,13 +2,17 @@
   <div class="comment_item bg-white mb-5 p-5 rounded-lg border-2 border-gray-300">
     <div class="relative">
       <div class="flex items-center">
-        <img
-          class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
-          :src="comment.image"
-          alt="Facebook profile image"
-        />
+        <nuxt-link :to="{name: 'profile-id', params: {id: comment.user_id}}">
+          <img
+            class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
+            :src="comment.image"
+            alt="Facebook profile image"
+          />
+        </nuxt-link>
         <div>
-          <h1 class="font-semibold">{{comment.first_name}} {{comment.last_name}}</h1>
+          <nuxt-link :to="{name: 'profile-id', params: {id: comment.user_id}}">
+            <h1 class="font-semibold">{{comment.first_name}} {{comment.last_name}}</h1>
+          </nuxt-link>
           <div class="flex items-center">
             <p class="font-semibold text-xs mr-2">{{formatDate(comment.created_at)}}</p>
           </div>
--- components/PostItem.vue
+++ components/PostItem.vue
@@ -2,24 +2,28 @@
   <div class="post_item mb-5 border-2 border-gray-300 rounded-lg p-5 bg-white">
     <div class="relative">
       <div class="flex items-center">
-        <img
-          v-if="post.image"
-          class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
-          :src="post.image"
-          alt="Facebook profile image"
-        />
-        <img
-          v-else
-          class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
-          :src="user.image"
-          alt="My Facebook profile image"
-        />
+        <nuxt-link :to="href">
+          <img
+            v-if="post.image"
+            class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
+            :src="post.image"
+            alt="Facebook profile image"
+          />
+          <img
+            v-else
+            class="rounded-full w-12 h-12 mr-2 border-2 border-blue-300"
+            :src="user.image"
+            alt="My Facebook profile image"
+          />
+        </nuxt-link>
         <div>
-          <h1
-            v-if="post.first_name && post.last_name"
-            class="font-semibold"
-          >{{post.first_name}} {{post.last_name}}</h1>
-          <h1 v-else class="font-semibold">{{user.first_name}} {{user.last_name}}</h1>
+          <nuxt-link :to="href">
+            <h1
+              v-if="post.first_name && post.last_name"
+              class="font-semibold"
+            >{{post.first_name}} {{post.last_name}}</h1>
+            <h1 v-else class="font-semibold">{{user.first_name}} {{user.last_name}}</h1>
+          </nuxt-link>
           <div class="flex items-center">
             <p class="font-semibold text-xs mr-2">{{formatDate(post.created_at)}}</p>
             <img
@@ -76,6 +80,15 @@ export default {
         return this.$auth.user.id === this.post.user_id
       } else {
         return true
+      }
+    },
+    href() {
+      if (this.$route.name === 'profile') {
+        return { name: this.$route.name }
+      } else if (this.$route.name === 'profile-id') {
+        return { name: this.$route.name, params: { id: this.$route.params.id } }
+      } else {
+        return { name: 'profile-id', params: { id: this.post.user_id } }
       }
     }
   },
