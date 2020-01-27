b5b67f2113b3a55c4be289fea5ed145f4a9dede3
bokismoki <borisbosnjak084@gmail.com>
2020-01-27 15:14:46 +0100

    Heroku deploy

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -2,14 +2,6 @@ const mysql = require('mysql')
 
 require('dotenv').config()
 
-const mysqlConnection = mysql.createConnection(process.env.DB_CONNECT_URI)
-
-mysqlConnection.connect(err => {
-    if(err) {
-        console.log('Error while connecting to the database', err)
-    } else {
-        console.log('Connected to the database')
-    }
-})
+const mysqlConnection = mysql.createPool(process.env.DB_CONNECT_URI)
 
 module.exports = mysqlConnection
