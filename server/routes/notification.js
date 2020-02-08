dd8eadffce9a85976c17e22b11ded4f107b1f705
bokismoki <borisbosnjak084@gmail.com>
2020-02-08 11:57:38 +0100

    Authorization middleware on notification routes

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,11 +1,13 @@
+const authorization = require('../routes/authorization')
+
 const router = require('express').Router()
 
 const notificationController = require('../controllers/notification')
 
-router.get('/get/by_user/:id', notificationController.getByUser)
+router.get('/get/by_user/:id', authorization, notificationController.getByUser)
 
-router.post('/add', notificationController.add)
+router.post('/add', authorization, notificationController.add)
 
-router.delete('/delete/:id', notificationController.delete)
+router.delete('/delete/:id', authorization, notificationController.delete)
 
 module.exports = router
