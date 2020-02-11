0c0c6f5b85871b7ee285d0b2a96b46ce6281a096
bokismoki <borisbosnjak084@gmail.com>
2020-02-11 22:22:21 +0100

    Google Login button

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="login">
     <button
-      class="absolute flex items-center bg-blue-800 w-4/6 justify-center py-2 rounded-full max-w-xl shadow-xl hover:bg-blue-700 lg:w-1/2"
-      @click="login"
+      class="absolute flex items-center mb-16 bg-blue-800 w-4/6 justify-center py-2 rounded-full max-w-xl shadow-xl hover:bg-blue-700 lg:w-1/2"
+      @click="loginFacebook"
     >
       <span class="mr-2 uppercase text-white">Log In</span>
       <span>
         <img class="w-8 rounded" src="~/assets/img/facebook.svg" alt="Facebook icon" />
+      </span>
+    </button>
+    <button
+      class="absolute flex items-center mt-16 bg-blue-800 w-4/6 justify-center py-2 rounded-full max-w-xl shadow-xl hover:bg-blue-700 lg:w-1/2"
+      @click="loginGoogle"
+    >
+      <span class="mr-2 uppercase text-white">Log In</span>
+      <span>
+        <img class="w-8 rounded" src="~/assets/img/google.svg" alt="Google icon" />
       </span>
     </button>
     <div class="bg-blue-200 px-4 py-2 inline-block">
@@ -14,7 +23,6 @@
       <p class="text-sm font-semibold">Email: srjivqxzmy_1580048820@tfbnw.net</p>
       <p class="text-sm font-semibold">Password: DanielAlisonsen99</p>
     </div>
-    <div @click="loginGoogle">CLICK ME TO LOGIN AS GOOGLE</div>
   </div>
 </template>
 
@@ -24,7 +32,7 @@ export default {
     title: 'Log In'
   },
   methods: {
-    login() {
+    loginFacebook() {
       this.$auth.loginWith('facebook')
     },
     loginGoogle() {
